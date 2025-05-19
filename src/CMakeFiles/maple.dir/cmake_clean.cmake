a78c9bd4780fc769c701867b7954be1b9a9deb4e
s-wyzy <sethwyzy@gmail.com>
2025-05-19 18:17:05 -0400

    initial commit

---
+++ src/CMakeFiles/maple.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maple.dir/main.cpp.o"
+  "CMakeFiles/maple.dir/main.cpp.o.d"
+  "maple"
+  "maple.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
