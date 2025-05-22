3389fc7539e6356a7bf20efc02570897e57632ee
s-wyzy <sethwyzy@gmail.com>
2025-05-22 19:18:33 -0400

    updated to work with new linux stuff

---
+++ build/CMakeFiles/maple.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maple.dir/src/main.cpp.o"
+  "CMakeFiles/maple.dir/src/raster.cpp.o"
+  "maple"
+  "maple.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
